033d065c101514fd1dcc7b3f78f16ee0ed30fdc2
hvu26510 <hvu26510@gmail.com>
2023-06-01 17:21:01 +0700

    Add project files.

---
+++ DemoKeThua/Hooman.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoKeThua
+{
+    internal class Hooman
+    {
+        protected string Name;
+        protected string Address;
+        protected string Email;
+        protected string PhoneNumber;
+
+        public Hooman(string name, string address, string email, string phoneNumber)
+        {
+            Name = name;
+            Address = address;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+
+        public virtual void Show()
+        {
+            Console.WriteLine("Ten: "+ this.Name);
+            Console.WriteLine("Dia chi: " + this.Address);
+            Console.WriteLine("Email: " + this.Email);
+            Console.WriteLine("SDT: " + this.PhoneNumber);
+        }
+
+    }
+
+    class NhanVien : Hooman
+    {
+        protected string bangCap;
+
+        public NhanVien(string name, string address, string email, string phoneNumber, string bangCap) : 
+            base(name, address, email, phoneNumber)
+        {
+            this.bangCap = bangCap;
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine("Ten: " + this.Name);
+            Console.WriteLine("Dia chi: " + this.Address);
+            Console.WriteLine("Email: " + this.Email);
+            Console.WriteLine("SDT: " + this.PhoneNumber);
+            Console.WriteLine("Bang cap: " + this.bangCap);
+
+        }
+
+    }
+
+
+    class KeToan : NhanVien
+    {
+        protected int Luong;
+        public KeToan(string name, string address, string email, string phoneNumber, string bangCap, int luong) : base(name, address, email, phoneNumber, bangCap)
+        {
+            this.Luong = luong;
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine("Ten: " + this.Name);
+            Console.WriteLine("Dia chi: " + this.Address);
+            Console.WriteLine("Email: " + this.Email);
+            Console.WriteLine("SDT: " + this.PhoneNumber);
+            Console.WriteLine("Bang cap: " + this.bangCap);
+            Console.WriteLine("Luong: " + this.Luong);
+
+        }
+    }
+}
---
+++ DemoKeThua/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoKeThua
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KeToan kt = new KeToan("Nguyen van A", "Ha Noi", "anv@gmail.com", "0123456789", "Tai chinh", 15000000);
+
+            kt.Show();
+
+            Console.ReadLine();
+        }
+    }
+}
